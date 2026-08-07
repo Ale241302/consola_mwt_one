77667ea4d39f5987cb8867ceb934590bd1a10d58
Alejandro Mendoza <alejandro@muitowork.com>
2026-08-07 09:26:58 -0500

    fix(portal): import IsAuthenticated en MwtUserViewSet

--- backend/apps/portal/views.py
+++ backend/apps/portal/views.py
@@ -30,6 +30,7 @@ from django.db.models import Q
 from django.utils import timezone
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from apps.core.permissions import IsCeoOrAdmin
